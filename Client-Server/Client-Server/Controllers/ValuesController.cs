d60428cd037461154daaeffd51e558152855d307
Fonki-00 <77162292+Fonki-00@users.noreply.github.com>
2022-05-12 16:20:29 +0300

    Add files via upload

---
+++ Client-Server/Client-Server/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
